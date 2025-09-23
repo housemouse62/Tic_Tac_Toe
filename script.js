d07e3218308c317113ea49e2cc8164968a7a4f3d
housemouse62 <ryan.skeels@gmail.com>
2025-09-23 23:41:04 +0200

    Using the connect4 as a blueprint for this go. Board is layed out and playable in console. Lots to do.

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
-console.log('hello world')
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const dropToken = (row, column, player) => {
+        const cellChosen = board[row][column];
+        if (cellChosen === '0') return;
+
+        board[row][column].addToken(player);
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, dropToken, printBoard};
+}
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
+}
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 'X'
+        },
+        {
+            name: playerTwoName,
+            token: 'O'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`${getActivePlayer} has played.`);
+        board.dropToken(row, column, getActivePlayer().token);
+
+        //check for winner & handle that logic including win message.
+
+    switchPlayerTurn();
+    printNewRound();
+};
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
