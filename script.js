2aced007a450144425994e20dbc276126adcfb29
housemouse62 <ryan.skeels@gmail.com>
2025-09-26 20:47:06 +0200

    Scoreboard is working. Need to prompt to try again if the cell already has a token.

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+const Scoreboard = (player1Name, player2Name) =>  {
+    
+    let scores = {[player1Name]: 0,
+                  [player2Name]: 0};
+    
+    const increment = (playerName) => {
+        scores[playerName]++
+    };
+
+    const reset = () => {
+        scores = {[player1Name]: 0, [player2Name]: 0};
+    };
+
+    const getScores = () => ({...scores});
+    const getPlayerNames = () => [player1Name, player2Name];
+
+    return { increment, reset, getScores, getPlayerNames };
+}
+
+function SetupGame() {
+    const playerOneName = playerOnePrompt();
+    const playerTwoName = playerTwoPrompt();
+
+    function playerOnePrompt() {
+        let playerOneName = prompt("Player One name?");
+        
+        if (playerOneName === null || playerOneName.trim() === '') {
+        playerOneName = 'Player One'
+        };
+        return playerOneName;
+    };
+
+    function playerTwoPrompt() {
+        let playerTwoName = prompt("Player Two name?");
+        
+        if (playerTwoName === null || playerTwoName.trim() === '') {
+        playerTwoName = 'Player Two'
+        };
+        return playerTwoName;
+    };
+
+    const scoreboard = Scoreboard(playerOneName, playerTwoName);
+    const game = GameController(scoreboard);
+
+    return { scoreboard, game };
+ 
+}
+
 function Gameboard() {
     const rows = 3;
     const columns = 3;
@@ -52,11 +100,10 @@ function Cell() {
     };
 }
 
-function GameController(
-    playerOneName = prompt("Player One name?"),
-    playerTwoName = prompt("Player Two name?")
-) {
+function GameController(scoreboard) {
     const gameboard = Gameboard();
+
+    const [playerOneName, playerTwoName] = scoreboard.getPlayerNames();
 
     const players = [
         {
@@ -70,18 +117,6 @@ function GameController(
     ];
 
     let currentPlayer = players[0];
-    let playerOneScore = 0;
-    let playerTwoScore = 0;
-
-    const Scoreboard = () => {
-        if (getCurrentPlayer().name === players[0].name) {
-            playerOneScore =+ 1
-        } else {
-            playerTwoScore =+ 1
-        }
-        console.log(`${players[0].name}: ${playerOneScore} | ${players[1].name}: ${playerTwoScore}`)
-     //  };
-     };
 
     const switchPlayerTurn = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
@@ -97,53 +132,48 @@ function GameController(
         let row = Math.floor((userInput-1) / 3);
         let column = ((userInput - 1) % 3);
         console.log(`${getCurrentPlayer().name} has played.`);
-    
-        gameboard.dropToken(row, column, getCurrentPlayer().token);  
-     //           console.log(`${players[0].name}: ${players[1].score} | ${players[1].name}: ${players[1].score}`)
-
-        console.log(getCurrentPlayer().name)
         
+        gameboard.dropToken(row, column, getCurrentPlayer().token);  
+  
         if (Checkwin(row, column)) { 
             if (PlayAgainQuestion()) {
-                Scoreboard(); 
-                ResetGame()} 
+                SetNextRound()} 
             } else if (Checkdraw()) {
-                alert('DRAW!');
-                if (PlayAgainQuestion()) { ResetGame() }
+                alert("It's a DRAW!");
+                if (PlayAgainQuestion()) { SetNextRound() }
             } else { switchPlayerTurn();
                      printNextRound(); }
     };     
 
     const Checkwin = (row, column) => {
-        console.log("row:", row, "column:", column);
         const rowCheck = gameboard.getBoard()[row].map(cell => cell.getValue());
         const columnCheck = gameboard.getBoard().map(row => row[column].getValue());
         const diagonalCheck = [gameboard.getBoard()[0][0].getValue(), gameboard.getBoard()[1][1].getValue(), gameboard.getBoard()[2][2].getValue()];
         const antiDiagonalCheck = [gameboard.getBoard()[2][0].getValue(), gameboard.getBoard()[1][1].getValue(), gameboard.getBoard()[0][2].getValue()];
         
         const isMarker = (currentMarker) => currentMarker === getCurrentPlayer().token;
-        
-        console.log(getCurrentPlayer().name);
-        console.log(currentPlayer.name);
-        console.log(players[0].name);
 
         if (rowCheck.every(isMarker)) {
-            console.log(`${getCurrentPlayer().name} has won!`);
+            PassScoreInfo();
+            AnnounceWinner();
             return true;
         };
 
         if (columnCheck.every(isMarker)) {
-            console.log(`${getCurrentPlayer().name} has won!`);
+            PassScoreInfo();
+            AnnounceWinner();
             return true;
         };
 
         if (diagonalCheck.every(isMarker)) {
-            console.log(`${getCurrentPlayer().name} has won!`);
+            PassScoreInfo();
+            AnnounceWinner();
             return true;
         };
 
         if (antiDiagonalCheck.every(isMarker)) {
-            console.log(`${getCurrentPlayer().name} has won!`);
+            PassScoreInfo();
+            AnnounceWinner();
             return true;
         };
 
@@ -153,10 +183,8 @@ function GameController(
     const Checkdraw = () => {
         const boardArray = gameboard.getBoard().map(row => row.map(cell => cell.getValue()));
         const flatBoard = boardArray.flat();
-        console.log(flatBoard);
         
         const isItMarked = (currentMarker) => currentMarker !== 0;
-            console.log(flatBoard.every(isItMarked));
             if (flatBoard.every(isItMarked)) {
                 console.log('draw')
                 return true;
@@ -165,22 +193,40 @@ function GameController(
         return false;
     };
 
+    const PassScoreInfo = () => {
+        scoreboard.increment(getCurrentPlayer().name);
+        console.log(scoreboard.getScores());
+    };
+
+    const AnnounceWinner = () => {
+        console.log(`${getCurrentPlayer().name} has won!`);
+    };
+
     const PlayAgainQuestion = () => {
         while (true) {
         const userAnswer = prompt('Would you like to play again? Y / N?');
-        console.log(userAnswer)
+
+        if (userAnswer === null) continue;
         if (userAnswer.toUpperCase() !== 'Y' && userAnswer.toUpperCase() !== 'N') continue;
         if (userAnswer.toUpperCase() === 'Y') return true;
         if (userAnswer.toUpperCase() === 'N') return false;
     }};
 
-    const ResetGame = () => {
+    //Use to ResetGameCompletely
+    // const ResetGame = () => {
+    //     gameboard.resetBoard();
+    //     currentPlayer = players[1];
+    //     scoreboard.reset();
+    //     console.log(scoreboard.getScores())
+    //     switchPlayerTurn();
+    //     printNextRound();
+    //     console.log('I reset')
+    // };
+
+    const SetNextRound = () => {
         gameboard.resetBoard();
-        currentPlayer = players[1];
-        switchPlayerTurn();
         printNextRound();
-        console.log('I reset')
-    };
+    }
 
     printNextRound();
 
@@ -188,6 +234,6 @@ function GameController(
         playRound,
         getCurrentPlayer
     };
-}
+};
 
-const game = GameController();
+const { game, scoreboard } = SetupGame();
